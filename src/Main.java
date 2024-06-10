1916547a7ff4ee4369857a378673fdfaad30102b
Solymar Quispe <solymar.quispe@tecsup.edu.pe>
2024-06-10 17:22:44 -0500

    Proyecto terminado v1

--- src/Main.java
+++ src/Main.java
@@ -1,16 +1,35 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+import java.io.IOException;
+import java.util.Map;
+import java.util.Scanner;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-            //prueba
+public class Main {
+    public Main() {
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        Consulta consulta1 = new Consulta();
+
+        try {
+            DatosConsulta datos = consulta1.datosConsulta();
+            System.out.println("Moneda Base: " + datos.getBase_code());
+            System.out.println("Conversiones Disponibles: " + datos.getConversion_rates().keySet());
+            System.out.print("Ingrese el monto en " + datos.getBase_code() + ": ");
+            double amount = sc.nextDouble();
+            System.out.print("Ingrese la moneda a convertir: ");
+            String targetCurrency = sc.next().toUpperCase();
+            Map<String, Object> conversionRates = datos.getConversion_rates();
+            if (conversionRates.containsKey(targetCurrency)) {
+                double rate = ((Number)conversionRates.get(targetCurrency)).doubleValue();
+                double convertedAmount = amount * rate;
+                System.out.printf("%.2f %s es equivalente a %.2f %s%n", amount, datos.getBase_code(), convertedAmount, targetCurrency);
+            } else {
+                System.out.println("Moneda a convertir es inválida.");
+            }
+        } catch (InterruptedException | IOException var12) {
+            Exception e = var12;
+            e.printStackTrace();
         }
+
     }
 }
